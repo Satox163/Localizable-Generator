e139011be8169a89cb7909b9ee5239fe19294c38
dmitriy.anokhin <rein1630@gmail.com>
2021-10-29 21:11:31 +0300

    fixed updated files and fixed csv parser

--- Sources/LocalizableGenerator/CSVParser.swift
+++ Sources/LocalizableGenerator/CSVParser.swift
@@ -28,7 +28,18 @@ func parser(_ csv: String) -> Result<[String: [LocalizedModel]], Error> {
         localizables[String(subs)] = []
     }
     rows[1...].forEach { subs in
-        let row = subs.split(separator: ",")
+        let row = subs.enumerated().split { (index, character) in
+            let nextIndex = subs.index(subs.startIndex, offsetBy: index + 1)
+            if subs.indices.contains(nextIndex) {
+                let nextString = subs[nextIndex]
+                if character == "," {
+                    return nextString != " "
+                }
+            }
+            return false
+        }.map { element in
+            String(element.map { $0.element }).replacingOccurrences(of: "\"", with: "")
+        }
         guard row.isEmpty == false else {
             return
         }
@@ -36,8 +47,11 @@ func parser(_ csv: String) -> Result<[String: [LocalizedModel]], Error> {
         languages
             .enumerated()
             .forEach { (index, lang) in
-                let model = LocalizedModel(key: String(key), value: String(row[index + 1]))
-                localizables[String(lang)]?.append(model)
+                let valueIndex = index + 1
+                if row.indices.contains(valueIndex) {
+                    let model = LocalizedModel(key: String(key), value: String(row[valueIndex]))
+                    localizables[String(lang)]?.append(model)
+                }
             }
     }
     return .success(localizables)
--- Sources/LocalizableGenerator/FileGenerator.swift
+++ Sources/LocalizableGenerator/FileGenerator.swift
@@ -9,9 +9,18 @@ func fileGenerator(data: [String: [LocalizedModel]]) throws {
             }
             .joined(separator: "\n")
         let directoryPath = "\(fileManager.currentDirectoryPath)/\(key).lproj"
+        var isDir: ObjCBool = true
+        if fileManager.fileExists(atPath: directoryPath, isDirectory: &isDir) {
+            try fileManager.removeItem(atPath: directoryPath)
+        }
         try fileManager.createDirectory(atPath: directoryPath, withIntermediateDirectories: false, attributes: [:])
+        let filePath = "\(directoryPath)/Localizable.strings"
+        isDir = false
+        if fileManager.fileExists(atPath: filePath, isDirectory: &isDir) {
+            try fileManager.removeItem(atPath: filePath)
+        }
         fileManager.createFile(
-            atPath: "\(directoryPath)/Localizable.strings",
+            atPath: filePath,
             contents: localizabeText.data(using: .utf8),
             attributes: [:]
         )
